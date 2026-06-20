6731328dc6ffbd8c9562113f95e996e201213a44
Bartosz Kosno <mail@ketrax.ovh>
2026-06-20 02:15:14 -0700

    feat: improve main menu

---
+++ Motion.md
@@ -0,0 +1,34 @@
+# Motion
+
+Gra na hackathon Horizons Nexus.
+
+Dwuosobowa gra co-op podwodna w stylu retro pixelart. Tekstury będą customowe i zrobione ręcznie 16x16. Czcionka Cairopixel.
+
+# Features
+
+This needs to be a motion game and feature the Microsoft Clippy in tutorial (tell the player the controls etc.). Game needs to be focused on satisfaction from playing and contain QOL features, simple flashes on impact and other graphic elements that support good gameplay
+
+## Menu
+
+Gra rozpoczyna sie widokiem na łódź i morze, wyżej od łodzi znajduje się logo rozgrywki. Gra jest 2d. Pod łodzią znajduje się pole do wpisania danych potrzebnych do połączenia się do rozgrywki kooperacyjnej wieloosobowej. Pole to, w którym znajduje się pole do wpisania danych jest jako "text bubble" ktore mowi "Clippy" (Prosze tutaj zostawic pole po lewo na wstawienie sprite'a Clippiego) w stylu retro 2d. Po wpisaniu danych pole do tekstu znika w stylu retro i z łodzi wychodzi pierwszy z z awodników (To gracz host), po tym gdy połączy się drugi zawodnik wychodzi z łodzi drugi zawodnik i wtedy pojawia  się opcja ready up, po podjeciu ready up, odpala sie animacja w ktorej gracze rownoczesnie wskakuja do wody robiac salto i plynnie przechodzi do rozgrywki 
+
+## Gra
+
+Przejście do sklepu (zbiorniki na tlen, abilities)  
+Rozpoczęcie gry na łodzi na górze   
+2 postacie zeskakują do wody  
+Pasek tlenu i głębokości
+Ławica ryb blokuje przejście (lewo -> prawo płynie dużo ryb)  
+Spoty do odładowania tlenu (butle z tlenem)
+Celem gry jest przejście poziomu. 
+Poziomy są generowane na podstawie losowego seeda i mają stałą głębokość.
+Postacie muszą unikać przeszkód, a jeśli jedna umrze to po kilku sekundach może się odrodzić.
+
+## Deathscreen
+
+Przy śmierci płynne przejście do głównego menu po krótkiej chwilce.
+
+## Winscreen
+
+Czas  
+Płynne przejście do głównego menu po kliknięciu guzika.
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,34 +1,72 @@
 extends Node2D
 
-@onready var titlecard: Sprite2D = $titlecard
-@onready var titletext: Label = $titletext
+@onready var main_panel: PanelContainer = $CanvasLayer/VBoxContainer/UIContainer/MainPanel
+@onready var lobby_panel: PanelContainer = $CanvasLayer/VBoxContainer/UIContainer/LobbyPanel
+@onready var settings_panel: PanelContainer = $CanvasLayer/VBoxContainer/UIContainer/SettingsPanel
+@onready var ui_container: CenterContainer = $CanvasLayer/VBoxContainer/UIContainer
 
-@onready var main_panel: PanelContainer = $CanvasLayer/CenterContainer/MainPanel
-@onready var lobby_panel: PanelContainer = $CanvasLayer/CenterContainer/LobbyPanel
+@onready var boat_player_1: ColorRect = $CanvasLayer/VBoxContainer/WorldContainer/Boat/Player1
+@onready var boat_player_2: ColorRect = $CanvasLayer/VBoxContainer/WorldContainer/Boat/Player2
 
-@onready var name_input: LineEdit = $CanvasLayer/CenterContainer/MainPanel/MarginContainer/VBoxContainer/NameBox/NameInput
-@onready var ip_input: LineEdit = $CanvasLayer/CenterContainer/MainPanel/MarginContainer/VBoxContainer/IPBox/IPInput
-@onready var host_button: Button = $CanvasLayer/CenterContainer/MainPanel/MarginContainer/VBoxContainer/ButtonsBox/HostButton
-@onready var join_button: Button = $CanvasLayer/CenterContainer/MainPanel/MarginContainer/VBoxContainer/ButtonsBox/JoinButton
-@onready var status_label: Label = $CanvasLayer/CenterContainer/MainPanel/MarginContainer/VBoxContainer/StatusLabel
+@onready var name_input: LineEdit = $CanvasLayer/VBoxContainer/UIContainer/MainPanel/MarginContainer/VBoxContainer/NameBox/NameInput
+@onready var ip_input: LineEdit = $CanvasLayer/VBoxContainer/UIContainer/MainPanel/MarginContainer/VBoxContainer/IPBox/IPInput
+@onready var host_button: Button = $CanvasLayer/VBoxContainer/UIContainer/MainPanel/MarginContainer/VBoxContainer/ButtonsBox/HostButton
+@onready var join_button: Button = $CanvasLayer/VBoxContainer/UIContainer/MainPanel/MarginContainer/VBoxContainer/ButtonsBox/JoinButton
+@onready var settings_button: Button = $CanvasLayer/VBoxContainer/UIContainer/MainPanel/MarginContainer/VBoxContainer/ButtonsBox/SettingsButton
+@onready var status_label: Label = $CanvasLayer/VBoxContainer/UIContainer/MainPanel/MarginContainer/VBoxContainer/StatusLabel
 
-@onready var player_list_label: Label = $CanvasLayer/CenterContainer/LobbyPanel/MarginContainer/VBoxContainer/PlayerList
-@onready var start_button: Button = $CanvasLayer/CenterContainer/LobbyPanel/MarginContainer/VBoxContainer/StartButton
-@onready var leave_button: Button = $CanvasLayer/CenterContainer/LobbyPanel/MarginContainer/VBoxContainer/LeaveButton
+@onready var player_list_label: Label = $CanvasLayer/VBoxContainer/UIContainer/LobbyPanel/MarginContainer/VBoxContainer/PlayerList
+@onready var start_button: Button = $CanvasLayer/VBoxContainer/UIContainer/LobbyPanel/MarginContainer/VBoxContainer/ButtonsBox/StartButton
+@onready var leave_button: Button = $CanvasLayer/VBoxContainer/UIContainer/LobbyPanel/MarginContainer/VBoxContainer/ButtonsBox/LeaveButton
+@onready var settings_back_button: Button = $CanvasLayer/VBoxContainer/UIContainer/SettingsPanel/MarginContainer/VBoxContainer/SettingsBackButton
 
-func _ready() -> void:
-	titlecard.visible = false
-	titletext.visible = false
-	
+@onready var resolution_btn: OptionButton = $CanvasLayer/VBoxContainer/UIContainer/SettingsPanel/MarginContainer/VBoxContainer/ResolutionBox/ResolutionBtn
+@onready var fullscreen_check: CheckBox = $CanvasLayer/VBoxContainer/UIContainer/SettingsPanel/MarginContainer/VBoxContainer/FullscreenBox/FullscreenCheck
+@onready var volume_slider: HSlider = $CanvasLayer/VBoxContainer/UIContainer/SettingsPanel/MarginContainer/VBoxContainer/VolumeBox/VolumeSlider
+
+
+
+func _ready() -> void:	
 	name_input.text = "Diver_" + str(randi() % 999)
 	
 	host_button.pressed.connect(_on_host_pressed)
 	join_button.pressed.connect(_on_join_pressed)
+	settings_button.pressed.connect(_on_settings_pressed)
 	start_button.pressed.connect(_on_start_pressed)
 	leave_button.pressed.connect(_on_leave_pressed)
+	settings_back_button.pressed.connect(_on_settings_back_pressed)
 	
 	MultiplayerManager.player_list_changed.connect(_on_player_list_changed)
 	MultiplayerManager.connection_status.connect(_on_connection_status)
+	
+	# Setup Resolution OptionButton
+	resolution_btn.clear()
+	resolution_btn.add_item("1280x720")
+	resolution_btn.add_item("1600x900")
+	resolution_btn.add_item("1920x1080")
+	resolution_btn.item_selected.connect(_on_resolution_selected)
+	
+	var current_size = DisplayServer.window_get_size()
+	if current_size == Vector2i(1280, 720):
+		resolution_btn.selected = 0
+	elif current_size == Vector2i(1600, 900):
+		resolution_btn.selected = 1
+	elif current_size == Vector2i(1920, 1080):
+		resolution_btn.selected = 2
+	else:
+		resolution_btn.selected = 0
+		
+	# Fullscreen CheckBox
+	var current_mode = DisplayServer.window_get_mode()
+	fullscreen_check.button_pressed = (current_mode == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN or current_mode == DisplayServer.WINDOW_MODE_FULLSCREEN)
+	fullscreen_check.toggled.connect(_on_fullscreen_toggled)
+	
+	# Volume Slider
+	var master_bus_idx = AudioServer.get_bus_index("Master")
+	var vol_db = AudioServer.get_bus_volume_db(master_bus_idx)
+	volume_slider.value = db_to_linear(vol_db)
+	volume_slider.value_changed.connect(_on_volume_changed)
+
 
 func _on_host_pressed() -> void:
 	var name_text = name_input.text.strip_edges()
@@ -59,6 +97,7 @@ func _on_leave_pressed() -> void:
 	main_panel.visible = true
 	start_button.visible = false
 	status_label.text = "Status: Disconnected"
+	_on_player_list_changed()
 
 func _on_player_list_changed() -> void:
 	var list_text = "Players connected:\n"
@@ -67,6 +106,10 @@ func _on_player_list_changed() -> void:
 		var suffix = " (Host)" if peer_id == 1 else ""
 		list_text += "- " + p_info["name"] + suffix + "\n"
 	player_list_label.text = list_text
+	
+	# Update player boxes visibility on the boat deck
+	boat_player_1.visible = 1 in MultiplayerManager.players
+	boat_player_2.visible = MultiplayerManager.players.size() > 1
 	
 	if multiplayer.multiplayer_peer != null:
 		main_panel.visible = false
@@ -78,3 +121,39 @@ func _on_connection_status(success: bool, message: String) -> void:
 		lobby_panel.visible = false
 		main_panel.visible = true
 		start_button.visible = false
+		_on_player_list_changed()
+
+
+func _on_settings_pressed() -> void:
+	main_panel.visible = false
+	settings_panel.visible = true
+
+func _on_settings_back_pressed() -> void:
+	settings_panel.visible = false
+	main_panel.visible = true
+
+func _on_resolution_selected(index: int) -> void:
+	var size = Vector2i(1280, 720)
+	match index:
+		0:
+			size = Vector2i(1280, 720)
+		1:
+			size = Vector2i(1600, 900)
+		2:
+			size = Vector2i(1920, 1080)
+	DisplayServer.window_set_size(size)
+	
+	# Center window on screen
+	var screen = DisplayServer.window_get_current_screen()
+	var screen_rect = DisplayServer.screen_get_usable_rect(screen)
+	DisplayServer.window_set_position(screen_rect.position + (screen_rect.size - size) / 2)
+
+func _on_fullscreen_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+func _on_volume_changed(value: float) -> void:
+	var master_bus_idx = AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_volume_db(master_bus_idx, linear_to_db(value))
